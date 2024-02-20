1900d87b2635d486dbf3652b9a11ebfa4614aeae
czar991 <shedrin389@gmail.com>
2024-02-20 12:59:32 +0300

    changes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -3,8 +3,8 @@ import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact, FaPenNib, FaAnchor } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
 import chechBets from '../../public/bets.png';
-import rmtdevImg from '../../public/rmtdev.png';
-import wordanalyticsImg from '../../public/wordanalytics.png';
+import cargo from '../../public/cargo.png';
+import docsapp from '../../public/docsapp.png';
 
 export const links = [
   {
@@ -75,18 +75,18 @@ export const projectsData = [
     url: 'https://check-bets.online/',
   },
   {
-    title: 'rmtDev',
-    description: 'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
-    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
-    url: '#!',
+    title: 'CargoGenius',
+    description: 'Мейн проект этого портфолио!Я работал фулстэк-разработчиком. Проект - интернет магазин с корзиной, пагинацией, фильтрацией, сортировкой, поиском и т.д',
+    tags: ['React', 'TypeScript', 'Next.js', 'SCSS', 'Redux', 'NestJS', 'Sequelize ORM', 'MySQL'],
+    imageUrl: cargo,
+    url: 'https://cargo-client-production.up.railway.app',
   },
   {
-    title: 'Word Analytics',
-    description: 'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
-    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
-    url: '#!',
+    title: 'DocsApp',
+    description: 'Фулстэк. Пет-проект для создания заметок!',
+    tags: ['React', 'Next.js', 'Tailwind', 'FramerJs'],
+    imageUrl: docsapp,
+    url: 'https://docsapp-tau.vercel.app',
   },
 ] as const;
 
